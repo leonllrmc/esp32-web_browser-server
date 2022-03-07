1af3bead78c783ca4a1523d2305b812f0a39a4d6
leonllrmc <leonllrcraft@gmail.com>
2022-03-07 07:56:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -63,4 +63,7 @@ app.get('/screen', async function(req, res) {
 
 
 
-app.listen(8080)
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
